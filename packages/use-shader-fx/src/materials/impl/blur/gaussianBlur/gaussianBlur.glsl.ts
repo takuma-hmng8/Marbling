e4d8cb280c5d7df4e22f59d6f5482cfdf961859c
roku-ft <r.jokura@funtech.inc>
2024-12-19 18:15:57 +0900

    fix glsl

--- packages/use-shader-fx/src/materials/impl/blur/gaussianBlur/gaussianBlur.glsl.ts
+++ packages/use-shader-fx/src/materials/impl/blur/gaussianBlur/gaussianBlur.glsl.ts
@@ -10,9 +10,9 @@ export const fragment = `
 	precision highp int;
 
 	// uniform sampler2D src;			
-	uniform vec2 u_stepSize;	
+	uniform vec2 stepSize;	
 	uniform vec2 sigma;	
-	uniform float u_weights[KERNEL_SIZE];	
+	uniform float weights[KERNEL_SIZE];	
 
 	void main() {
 
@@ -25,13 +25,13 @@ export const fragment = `
 		float w;
 		float sumW = 0.0;
 		float actualWeight;
-		vec2 stepSize = u_stepSize * sigma;
+		vec2 stepSize = stepSize * sigma;
 		
 		vec2 uv = (renderCount == 0) ? vTextureCoverUv : vUv;		
 		
 		for(int i = 0; i <  KERNEL_SIZE - 1; i++){
 
-			w = u_weights[i];			
+			w = weights[i];			
 			color = texture2D( texture_src, uv - count * texelSize * stepSize );
 			actualWeight = w * color.a;
 			sum.rgb += color.rgb * actualWeight;
@@ -47,7 +47,7 @@ export const fragment = `
 			count--;
 		}
 
-		w = u_weights[KERNEL_SIZE - 1];
+		w = weights[KERNEL_SIZE - 1];
 
 		color = texture2D( texture_src, uv );
 		actualWeight = w * color.a;
