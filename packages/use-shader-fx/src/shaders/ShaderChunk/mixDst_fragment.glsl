61691e1b08853a67f72147ba0fbea46dbfc2d582
takuma-hmng8 <t.hashimoto@funtech.inc>
2025-01-28 21:10:28 +0900

    update

--- packages/use-shader-fx/src/shaders/ShaderChunk/mixDst_fragment.glsl
+++ packages/use-shader-fx/src/shaders/ShaderChunk/mixDst_fragment.glsl
@@ -1,36 +1,21 @@
 #ifdef USF_USE_MIXDST
 
-	// mix uv
+	// uv
 	vec2 mixedUv = vMixDstCoverUv;
-	
-	if(mixDst_uv){
-
-		float circlePower = calcMixCirclePower(mixDst_uv_position,mixDst_uv_radius) * mixDst_uv_factor;
-		mixedUv += (mixDst_uv_offset + (usf_FragColor.rg * 2.0 - 1.0)) * circlePower;
-
-	}
-
+	mixedUv += mixDst_uv 
+		? (mixDst_uv_offset + (usf_FragColor.rg * 2. - 1.)) * calcMixCirclePower(mixDst_uv_position,mixDst_uv_radius) * mixDst_uv_factor
+		: vec2(0.);
 	vec4 mixDstColor = texture2D(mixDst_src, mixedUv);
 
 	// color
-	if(mixDst_color){
-
-		float circlePower = calcMixCirclePower(mixDst_color_position,mixDst_color_radius) * mixDst_color_factor;
-		usf_FragColor = mix(usf_FragColor, mixDstColor, circlePower);
-
-	}
+	usf_FragColor = mixDst_color 
+		? mix(usf_FragColor, mixDstColor, calcMixCirclePower(mixDst_color_position,mixDst_color_radius) * mixDst_color_factor) 
+		: usf_FragColor;
 
 	// alpha
-	if(mixDst_alpha){
-
-		float circlePower = calcMixCirclePower(mixDst_alpha_position,mixDst_alpha_radius) * mixDst_alpha_factor;
-		usf_FragColor = mix(usf_FragColor, mixDstColor, mixDstColor.a * circlePower);
-
-	}
+	usf_FragColor = mixDst_alpha 
+		? mix(usf_FragColor, mixDstColor, mixDstColor.a * calcMixCirclePower(mixDst_alpha_position,mixDst_alpha_radius) * mixDst_alpha_factor)
+		: usf_FragColor;
 	
 #endif
-
-
-
-
 
